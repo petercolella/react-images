5ab0297cfa150c9fa68c9363eb822a70cba82aef
petercolella <peter.joseph.colella@gmail.com>
2021-01-28 01:11:13 -0500

    adds cards.json and updates PublicFolderImage

--- src/containers/PublicFolderImage/PublicFolderImage.jsx
+++ src/containers/PublicFolderImage/PublicFolderImage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Card from "../../components/Card/Card";
+import cards from "./cards.json";
 
 const PublicFolderImage = () => {
   return (
     <div>
       <h1>Images from Public Folder</h1>
+      {cards.map((card, i) => (
+        <Card key={i} {...card} />
+      ))}
     </div>
   );
 };
