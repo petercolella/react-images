fa65123780aa6d6b15f6e058c1608aa88f1f5988
petercolella <peter.joseph.colella@gmail.com>
2021-01-29 20:03:37 -0500

    replaces wildcard route with Redirect in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import PublicFolderImages from "./containers/PublicFolderImages/PublicFolderImages";
@@ -14,12 +19,8 @@ function App() {
           <Route exact path="/home" component={Home} />
           <Route exact path="/public" component={PublicFolderImages} />
           <Route exact path="/src" component={SrcFolderImages} />
-          <Route
-            exact
-            path={"/src-require"}
-            component={SrcFolderRequireImages}
-          />
-          <Route path="*" component={Home} />
+          <Route exact path="/src-require" component={SrcFolderRequireImages} />
+          <Redirect to="/home" />
         </Switch>
       </div>
     </Router>
