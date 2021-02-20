0e7b80ed65a34199d0fe23ab9cd6a7ae2463b53d
petercolella <peter.joseph.colella@gmail.com>
2021-02-19 20:01:01 -0500

    updates styling and text of Home container

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,9 +2,24 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>React Images</h1>
-    </div>
+    <>
+      <div className="row my-5">
+        <div className="col">
+          <h1 className="text-center">React Images</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col">
+          <p>
+            This app demonstrates three methods to render images when they are
+            part of a data set being mapped over.
+          </p>
+          <p>Click on the links above for an explanation of each one.</p>
+        </div>
+        <div className="col"></div>
+      </div>
+    </>
   );
 };
 
