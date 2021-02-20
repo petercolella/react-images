497bff1f89b5bf60c0821ff6c1bd1e4eded12629
petercolella <peter.joseph.colella@gmail.com>
2021-02-19 19:51:16 -0500

    updates Image component with new title prop

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Image = ({ src, alt }) => {
-  return <img className="card-img-top" src={src} alt={alt} />;
+const Image = ({ src, title }) => {
+  return <img className="card-img-top" src={src} alt={title} />;
 };
 
 export default Image;
