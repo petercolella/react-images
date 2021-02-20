f185a70c0929e379ae6698612d3a781f870baa24
petercolella <peter.joseph.colella@gmail.com>
2021-02-19 19:55:31 -0500

    updates card component with title prop

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,13 +3,11 @@ import Image from "../Image/Image";
 
 const Card = (props) => {
   return (
-    <div className="col">
+    <div className="p-3">
       <div className="card" style={{ width: "18rem" }}>
         <Image {...props} />
         <div className="card-body">
-          <p className="card-text">{`${props.alt
-            .substring(0, 1)
-            .toUpperCase()}${props.alt.substring(1)}`}</p>
+          <p className="card-text">{props.title}</p>
         </div>
       </div>
     </div>
