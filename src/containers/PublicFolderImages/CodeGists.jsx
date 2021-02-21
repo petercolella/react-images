4b49a26ef4447a24409a99aca71487d38f13c36f
petercolella <peter.joseph.colella@gmail.com>
2021-02-20 20:53:03 -0500

    adds react-gist package and CodeGists files

---
+++ src/containers/PublicFolderImages/CodeGists.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Gist from "react-gist";
+
+const CodeGists = () => {
+  return (
+    <div>
+      <Gist
+        id="3e39b09eb44a51b4b2535345426c53ad"
+        file={"PublicFolderImages.jsx"}
+      />
+      <Gist id="3e39b09eb44a51b4b2535345426c53ad" file={"cards.json"} />
+    </div>
+  );
+};
+
+export default CodeGists;
---
+++ src/containers/SrcFolderImages/CodeGists.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Gist from "react-gist";
+
+const CodeGists = () => {
+  return (
+    <div>
+      <Gist
+        id="3e39b09eb44a51b4b2535345426c53ad"
+        file={"SrcFolderImages.jsx"}
+      />
+    </div>
+  );
+};
+
+export default CodeGists;
